3c0a6b6cb828500adc5ba2f461a3892601566c32
yamada5131 <son.nl215131@gmail.com>
2024-08-03 22:14:35 +0700

    complete chapter4

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -29,6 +32,23 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    protected static function booted(): void
+    {
+        static::addGlobalScope('active', function (Builder $builder) {
+            $builder->where('is_active', 1);
+        });
+    }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function scopeAdmin(Builder $query): void
+    {
+        $query->where('is_admin', 1);
+    }
+
     /**
      * Get the attributes that should be cast.
      *
@@ -42,8 +62,17 @@ class User extends Authenticatable
         ];
     }
 
-    public function posts(): HasMany
+    protected function fullname(): Attribute
     {
-       return $this->hasMany(Post::class);
+        return Attribute::make(
+            get: fn(mixed $value, array $attributes) => $attributes['first_name'].' '.$attributes['last_name'],
+        );
+    }
+
+    protected function username(): Attribute
+    {
+        return Attribute::make(
+            set: fn(string $value) => Str::slug($value, '-'),
+        );
     }
 }
